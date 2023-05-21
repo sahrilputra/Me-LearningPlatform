6d63f49477afd0ffd5ba012cddddb96386d5e888
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-05-21 12:02:47 +0700

    add Delete Method

--- services-media/routes/media.js
+++ services-media/routes/media.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 const isBase64 = require("is-base64");
 const base64Img = require("base64-img");
-
+const fs = require("fs");
 const { Media } = require("../models");
 
 router.get("/", async (req, res) => {
   const media = await Media.findAll({
-    attributes : ['id', 'image']
+    attributes: ["id", "image"]
   });
 
-  const mappedMedia = media.map((m) => {
-    m.image = `${req.get('host')}/${m.image}`;
+  const mappedMedia = media.map(m => {
+    m.image = `${req.get("host")}/${m.image}`;
 
     return m;
-  })
+  });
 
   return res.json({
     status: "success",
@@ -34,20 +34,46 @@ router.post("/", (req, res) => {
     if (err) {
       return res.status(400).json({ status: "error", message: err.message });
     }
-  
+
     const filename = filepath.split("\\").pop().split("/").pop();
-  
+
     const media = await Media.create({ image: `images/${filename}` });
-  
+
     return res.json({
       status: "success",
       data: {
         id: media.id,
-        image: `${req.get('host')}/images/${filename}`
+        image: `${req.get("host")}/images/${filename}`
       }
+    });
+  });
+
+  router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const media = await Media.findByPk(id);
+
+    if (!media) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "media not found" });
+    }
+
+    fs.unlink(`./public/${media.image}`, async err => {
+      if (err) {
+        return res
+        .status(400)
+        .json({ status: "error", message: err.message });
+      }
+
+      await media.destroy();
+
+      return res.json({
+        status: "success",
+        message: "image deleted"
+      });
     });
   });
 });
 
 module.exports = router;
-
