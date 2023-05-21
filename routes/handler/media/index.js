aced2d50144d4edd5dd588b94c02afc849454542
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-05-21 22:01:57 +0700

    add delete Method Api

--- routes/handler/media/index.js
+++ routes/handler/media/index.js
@@ -1,7 +1,10 @@
 const create = require('./create');
 const getAll = require('./getAll');
+const destroy = require('./destory');
+
 
 module.exports = { 
     create,
-    getAll
+    getAll,
+    destroy
 }
--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const mediaHandler = require('./handler/media');
+const mediaHandler = require("./handler/media");
 
-
-router.post('/', mediaHandler.create);
-router.get('/', mediaHandler.getAll)
-
+router.post("/", mediaHandler.create);
+router.get("/", mediaHandler.getAll);
+router.delete("/id", mediaHandler.destroy);
 module.exports = router;
